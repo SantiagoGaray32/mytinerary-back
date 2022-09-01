6ca810242039e7ead24c94f20ebb4d49913fcc29
FranRomero209 <francromero@protonmail.com>
2022-09-01 02:02:33 -0300

    Controlador corregido y City.js con validador

--- routes/Cities.js
+++ routes/Cities.js
@@ -1,14 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { read, destroy, updateCity } = require('../controllers/cityController');
-const {create} = require('../controllers/cityController');
-const {all} = require('../controllers/cityController')
+const {
+  all,
+  create,
+  read,
+  destroy,
+  updateCity,
+} = require("../controllers/cityController");
 
-router.get('/',all)
-router.get('/:id',read)
-router.post('/', create)
-router.delete('/:id',destroy)
-router.put('/:id',updateCity)
+router.get("/", all);
+router.get("/:id", read);
+router.post("/", create);
+router.delete("/:id", destroy);
+router.put("/:id", updateCity);
 
-
-module.exports = router
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const cityRouter = require('./Cities')
+const cityRouter = require("./Cities");
+const userRouter = require("./users.js");
 
-router.get('/', function(req, res, next) {
-  res.render('index',{title: 'Express'} );
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.use('/cities',cityRouter)
+router.use("/cities", cityRouter);
+router.use("/users", userRouter);
 
 module.exports = router;
