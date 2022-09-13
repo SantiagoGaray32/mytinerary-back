a378eda82b60b3b5d113e43069478e3805856d29
FranRomero209 <francromero@protonmail.com>
2022-09-13 20:21:49 -0300

    user.js+signIn.js+auth.js

--- models/user.js
+++ models/user.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
+  photo: { type: String, required: true },
   email: { type: String, required: true },
   password: [{ type: String, required: true }],
-  userPhoto: { type: String, required: true },
+  code: { type: String, required: true },
   country: { type: String, required: true },
-  from: { type: Array },
-  uniqueString: { type: String, required: true },
+  from: [{ type: String, required: true }],
   verification: { type: Boolean, required: true },
+  logged: { type: Boolean, required: true },
+  role: { type: String, required: true },
 });
 
 const Users = mongoose.model("users", usersSchema);
