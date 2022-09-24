3226e8babea6d2967c148facb02cd3314a6d51cd
SantiagoGaray32 <santi.garay2811@gmail.com>
2022-09-24 12:49:28 -0300

    modificando rutas con passport

--- routes/comments.js
+++ routes/comments.js
@@ -1,16 +1,22 @@
 var express = require("express");
 var router = express.Router();
+var passport = require('../config/passport')
 
 const {
   createComment,
   all,
   readFromCities,
   readFromUser,
+  editComment,
+  deleteComment,
 } = require("../controllers/commentController");
 
+router.post("/",passport.authenticate('jwt', {session:false}), createComment);
 router.get("/", all); // comments?cityname=Paris
-router.get("/", readFromCities); //comments/4 (4 es el ID de la ciudad) => nunca se ejecuta
-router.get("", readFromUser); //nunca se ejecuta
-router.post("/", createComment);
+router.get('/user',readFromUser );
+router.patch('/:id',passport.authenticate('jwt', {session:false}),editComment);
+router.delete('/:id',passport.authenticate('jwt', {session:false}),deleteComment);
 
 module.exports = router;
+
+// las peticiones me dan un 401 (unahutorizade) tenemos que pasar el token que generamos al registrarnos.
