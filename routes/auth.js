af3e25d96420fcd83c4d243b113fa490bd5fd535
SantiagoGaray32 <santi.garay2811@gmail.com>
2022-09-24 12:46:48 -0300

    modificando rutas con passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,21 @@
 var express = require("express");
+const { session } = require("passport");
+const passport = require("../config/passport");
 var router = express.Router();
 const {
   signUp,
   verifyMail,
   signIn,
   signOut,
+  signInWithToken,
+  editProfile,
 } = require("../controllers/userController");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.get("verify/:code", verifyMail);
 router.put("/signOut/:id", signOut);
+router.get("/token", passport.authenticate('jwt',{session:false}), signInWithToken);
+router.patch('update/:id', editProfile)
 
 module.exports = router;
